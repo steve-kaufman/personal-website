4fea1e069923c961596551e497f90705a8905ac3
Leyla <leyla@pop-os.localdomain>
2020-02-10 12:26:58 -0600

    built

---
+++ react-ui/build/precache-manifest.d58b27b55d7a29bb67b2cb799d68a767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab9aaf35ce4b8d150ffbab4077735b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe26c7ba3425974a814",
+    "url": "/static/css/main.0c8c6597.chunk.css"
+  },
+  {
+    "revision": "26c2e08b567ddaef4cbd",
+    "url": "/static/js/2.ab01312e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ab01312e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbe26c7ba3425974a814",
+    "url": "/static/js/main.6941603e.chunk.js"
+  },
+  {
+    "revision": "5abfcef5aacaf22b5959",
+    "url": "/static/js/runtime-main.baee6d55.js"
+  }
+]);
